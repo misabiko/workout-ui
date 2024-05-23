2b3024c8a8f83716b1d6cb8fb546f3868942688d
misabiko <misabiko@gmail.com>
2024-05-23 18:51:46 -0400

    Null check spreadsheet result

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -16,24 +16,25 @@ export const load: PageServerLoad = async () => {
 		range: 'Log!A:A',
 	});
 
-	if (!datesRes.data.values) {
+	if (!datesRes.data.values)
 		error(500, 'Failed database date request.');
-	}
 
 	const today = (new Date()).toLocaleDateString("en-US");
 	const index = datesRes.data.values.findIndex(row => row[0] === today);
-	if (index === -1) {
+	if (index === -1)
 		error(500, `Failed database request for date '${today}'`);
-	}
 
 	const dataRes = await sheets.spreadsheets.values.get({
 		spreadsheetId: SPREADSHEET_ID,
 		range: `Log!B${index + 1}:F${index + 7}`,
 	});
 
+	if (!dataRes.data.values)
+		error(500, `Failed database request for date '${today}'`);
+
 	return {
 		date: today,
-		values: dataRes.data.values,
+		values: dataRes.data.values as string[][],
 		spreadsheetId: SPREADSHEET_ID,
 	};
 }
